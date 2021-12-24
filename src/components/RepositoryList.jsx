1aabf29bf40e28754fd1275b2853eb192984ee6a
Adriano Moreira <adr_0603@yahoo.com.br>
2021-12-23 22:48:01 -0300

    React Properties

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,33 +1,19 @@
-const repositoryName = 'unform2'
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: "unform",
+    description: "Forms in React",
+    link: 'https://github.com/unform/unform'
+}
 export function RepositoryList() {
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-
-                    <a href="http://">
-                        Acessar repositório no GitHub
-                    </a>
-                </li>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-
-                    <a href="http://">
-                        Acessar repositório no GitHub
-                    </a>
-                </li>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-
-                    <a href="http://">
-                        Acessar repositório no GitHub
-                    </a>
-                </li>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem />
+                <RepositoryItem />
+                <RepositoryItem />
             </ul>
         </section>
     );
